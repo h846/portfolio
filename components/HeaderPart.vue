fd510b68c641fb01552db7c952cf2533de6cac13
Hideki Yashiro <hideki.yashiro@landsend.co.jp>
2022-08-26 13:12:15 +0900

    body part

--- components/HeaderPart.vue
+++ components/HeaderPart.vue
@@ -3,25 +3,28 @@
 </script>
 
 <template>
-	<div>
-		<div class="q-pa-md q-gutter-md">
-			<div class="row justify-between">
-				<q-parallax :src="img_src">
-					<h1 class="text-white">Hideki Yashiro</h1>
-				</q-parallax>
-			</div>
-		</div>
+	<div class="wrapper">
+		<h1 class="title">Hideki Yashiro</h1>
 	</div>
 </template>
 
-<style lang="scss">
-	@import url("https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap");
+<style lang="scss" scoped>
+	.wrapper {
+		height: 800px;
+		background: linear-gradient(to bottom, transparent, #fff),
+			url("@/assets/sauna_bg.jpg");
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		background-color: #464646;
+	}
 	.title {
-		font-family: "Sawarabi Gothic", sans-serif !important;
 		font-style: italic;
+		color: white;
 		letter-spacing: 1px;
 		text-shadow: 0px 4px 3px rgba(0, 0, 0, 0.4), 0px 8px 13px rgba(0, 0, 0, 0.1),
 			0px 18px 23px rgba(0, 0, 0, 0.1);
-		font-size: 3rem;
+		font-size: 2rem;
 	}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>
+	<q-layout view="hHh lpR fFf" height="100%">
+		<q-header elevated class="bg-grey-3 text-black" height-hint="98">
+			<q-toolbar>
+				<q-toolbar-title> My Portfolio </q-toolbar-title>
+				<q-space />
+				<q-tabs align="right" shrink>
+					<q-route-tab label="Profile" />
+					<q-route-tab label="Works" />
+					<q-route-tab label="Contact" />
+				</q-tabs>
+			</q-toolbar>
+		</q-header>
 		<HeaderPart />
-	</div>
+		<!--<BodyPart />-->
+	</q-layout>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+	@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
+	html,
+	body {
+		font-family: "Noto Sans JP", sans-serif !important;
+	}
+</style>
