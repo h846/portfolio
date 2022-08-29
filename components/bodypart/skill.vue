5ef636c16e7130e291e06d1a8a1327ffd3f1ed43
h846 <h846@outlook.com>
2022-08-29 20:28:05 +0900

    add label to skill

--- components/bodypart/skill.vue
+++ components/bodypart/skill.vue
@@ -1,98 +1,76 @@
 <script setup lang="ts">
 	interface skillObj {
-		htmlcss: number;
-		JavaScript: number;
-		NodeJs: number;
-		Python: number;
-		RPA: number;
+		htmlcss: { title: string; val: number };
+		JavaScript: { title: string; val: number };
+		NodeJS: { title: string; val: number };
+		Python: { title: string; val: number };
+		RPA: { title: string; val: number };
 	}
 	const skills: skillObj = reactive({
-		htmlcss: 0,
-		JavaScript: 0.9,
-		NodeJs: 0.7,
-		Python: 0.6,
-		RPA: 0.8,
+		htmlcss: { title: "HTML5/CSS3", val: 0 },
+		JavaScript: { title: "JavaScript", val: 0 },
+		NodeJS: { title: "Node.js", val: 0 },
+		Python: { title: "Python", val: 0 },
+		RPA: { title: "RPA", val: 0 },
 	});
 
 	onMounted(() => {
-		skills.htmlcss = 1;
+		skills.htmlcss.val = 1;
+		skills.JavaScript.val = 0.9;
+		skills.NodeJS.val = 0.7;
+		skills.Python.val = 0.5;
+		skills.RPA.val = 0.8;
 	});
 </script>
 
 <template>
 	<div class="profile-wrapper">
 		<div>
-			<h1 class="title">SKILL</h1>
+			<div class="title">SKILL</div>
 			<div class="row">
-				<div class="col-6 q-pa-sm">
-					HTML5/CSS3
+				<div class="col-6 q-pa-sm" v-for="(i, k, idx) in skills" :key="idx">
+					{{ i.title }}
 					<q-linear-progress
 						stripe
-						size="20px"
+						size="25px"
 						color="warning"
 						rounded
-						:value="skills.htmlcss"
-					/>
-				</div>
-				<div class="col-6 q-pa-sm">
-					JavaScript
-					<q-linear-progress
-						stripe
-						size="20px"
-						color="warning"
-						rounded
-						:value="skills.JavaScript"
-					/>
-				</div>
-				<div class="col-6 q-pa-sm">
-					Node.js
-					<q-linear-progress
-						stripe
-						size="20px"
-						color="warning"
-						rounded
-						:value="skills.NodeJs"
-					/>
-				</div>
-				<div class="col-6 q-pa-sm">
-					Python
-					<q-linear-progress
-						stripe
-						size="20px"
-						color="warning"
-						rounded
-						:value="skills.Python"
-					/>
-				</div>
-
-				<div class="col-6 q-pa-sm">
-					RPA
-					<q-linear-progress
-						stripe
-						size="20px"
-						color="warning"
-						rounded
-						:value="skills.RPA"
-					/>
+						:value="i.val"
+					>
+						<div class="absolute-full flex flex-center">
+							<q-badge
+								color="white"
+								text-color="accent"
+								:label="i.val * 100 + '%'"
+							/>
+						</div>
+					</q-linear-progress>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.profile-wrapper {
 		font-family: "Roboto";
 		width: 100%;
 		height: 500px;
 		> div {
 			text-align: center;
+			max-width: 600px;
 			width: 80%;
 			margin: 0 auto;
 			.title {
+				text-align: center;
+				line-height: 20px;
+				width: 90px;
 				font-size: 2rem;
 				font-weight: bold;
 				margin: 0 auto;
+				padding: 0.5rem 0rem;
+				margin-bottom: 0.2rem;
+				border-bottom: 6px solid #f6ba33;
 			}
 		}
 	}
