1a674b2528c0ae64d24d4d2f81b6630d42f3436c
h846 <h846@outlook.com>
2022-08-30 20:56:08 +0900

    save

--- components/bodypart/work.vue
+++ components/bodypart/work.vue
@@ -2,11 +2,21 @@
 	interface works {
 		camptool: { title: string; desc: string; imgsrc: Array<string> };
 	}
+
+	const workData: works = {
+		camptool: {
+			title: "キャンペーン検索ツール",
+			desc: `現在有効のキャンペーンや過去のキャンペーンを検索、表示、管理できるツール。
+			OracleDBと連携したCRUD機能を有しています。検索時間の短縮、ユーザー目線に合わせたUI/UXの改善により
+			大幅な業務効率の改善に貢献しました。`,
+			imgsrc: [""],
+		},
+	};
 </script>
 
 <template>
 	<div class="row">
-		<div class="col-4" v-for="i in 3">
+		<div class="col-4" v-for="i in 3" :key="i">
 			<q-card class="work-card">
 				<q-card-section>
 					<img src="/assets" />
