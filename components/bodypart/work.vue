7546ac30b91d8e3ee137a4633c668c91fd9b9da2
Hideki Yashiro <hideki.yashiro@landsend.co.jp>
2022-09-02 13:10:09 +0900

    add modal window

--- components/bodypart/work.vue
+++ components/bodypart/work.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 	interface works {
-		camptool: { title: string; desc: string; imgsrc: Array<string> };
+		camptool: {
+			title: string;
+			desc: string;
+			imgsrc: Array<string>;
+			dialog: boolean;
+		};
 	}
 
 	const workData: works = reactive({
@@ -9,7 +14,12 @@
 			desc: `現在有効のキャンペーンや過去のキャンペーンを検索、表示、管理できるツール。
 			OracleDBと連携したCRUD機能を有しています。検索時間の短縮、ユーザー目線に合わせたUI/UXの改善により
 			大幅な業務効率の改善に貢献しました。`,
-			imgsrc: ["/img/campaign/1.png"],
+			imgsrc: [
+				"/img/campaign/1.png",
+				"/img/campaign/2.png",
+				"/img/campaign/3.png",
+			],
+			dialog: false,
 		},
 	});
 </script>
@@ -17,14 +27,42 @@
 <template>
 	<div class="row">
 		<div class="col-4" v-for="(i, k, idx) in workData" :key="idx">
-			<q-card class="work-card">
+			<!-- Card Part-->
+			<q-card class="work-card" @click="i.dialog = true">
 				<q-card-section>
 					<q-img :src="i.imgsrc[0]" />
 				</q-card-section>
 				<q-card-section class="q-pt-none">
-					{{ i.desc }}
+					{{ i.title }}
 				</q-card-section>
 			</q-card>
+			<!-- Dialog Part -->
+			<q-dialog
+				v-model="i.dialog"
+				transition-show="flip-down"
+				transition-hide="flip-up"
+			>
+				<q-card
+					class="q-pa-lg"
+					style="width: 100%; max-width: 700px; height: 400px"
+				>
+					<q-card-section class="q-pt-none">
+						<div class="row">
+							<div class="col-5">
+								<div>{{ i.title }}</div>
+								<div>{{ i.desc }}</div>
+							</div>
+							<div class="col-7">
+								<div class="row">
+									<div class="col" v-for="img in i.imgsrc">
+										<q-img :src="img" style="height: 100px" />
+									</div>
+								</div>
+							</div>
+						</div>
+					</q-card-section>
+				</q-card>
+			</q-dialog>
 		</div>
 	</div>
 </template>
@@ -33,5 +71,6 @@
 	.work-card{
 	  height:300px;
 	  min-width 300px;
+		cursor:pointer;
 	}
 </style>
